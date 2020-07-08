eae1e7855b17a84ba245b0b2df7d8183f4c126bb
maximsondv <maximsondv@gmail.com>
2020-07-09 00:30:29 +0600

    Date selector

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {CalendarComponent} from './calendar/calendar.component';
+import {SelectorComponent} from './selector/selector.component';
+import {OrganizerComponent} from './organizer/organizer.component';
+import {MomentPipe} from './shared/moment.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    SelectorComponent,
+    OrganizerComponent,
+    MomentPipe
   ],
   imports: [
     BrowserModule,
@@ -15,4 +23,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
